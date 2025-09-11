29f6c61af3cbc1e61ba0ba7a8ca1c1626bc6b7a9
David Nichols <david.c.nichols50@gmail.com>
2025-09-11 19:24:34 -0400

    sss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import type { AppProps } from 'next/app'
 import '../styles/global.css'
 // Import chart configuration to ensure Chart.js is properly registered
 import '../lib/chart-config'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
